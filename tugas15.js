07796692abec2cfe5f0c6b2c7d57581b9b71148b
sinar96 <sinar96>
2020-01-21 21:39:03 +0800

    math sqrt, pow

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function segitiga() {
+    var AB = 8;
+    console.log("Nilai AB : ", AB);
+    
+    var BC = 6;
+    console.log("Nilai AB : ", BC);
+
+    var AC = Math.sqrt((Math.pow(AB,2))+(Math.pow(BC,2)));
+    console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : ", AC, "cm");
+}
+segitiga();
